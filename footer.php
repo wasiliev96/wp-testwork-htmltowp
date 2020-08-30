e790422cc8117401ae751d152199f6fc39697e0d
wasiliev96 <wasiliev.cloud@gmail.com>
2020-08-30 16:24:11 +0300

    editable copyright added

--- footer.php
+++ footer.php
@@ -13,40 +13,41 @@
                 <strong class="strong-text">NUTZEN SIE IHRE CHANCE!</strong>
             </div>
             <div class="col-lg-2 col-md-3 col-sm-6 col-12">
-                <?php dynamic_sidebar( 'footer_area_one' ); ?>
-<!--                <div class="text label">Info</div>-->
-<!--                <ul>-->
-<!--                    <li><a href="" class="text">Kontakt </a></li>-->
-<!--                    <li><a href="" class="text">Impressum</a></li>-->
-<!--                    <li><a href="" class="text">Datenschutz</a></li>-->
-<!--                    <li><a href="" class="text">AGB</a></li>-->
-<!--                </ul>-->
+                <?php dynamic_sidebar('footer_area_one'); ?>
+                <!--                <div class="text label">Info</div>-->
+                <!--                <ul>-->
+                <!--                    <li><a href="" class="text">Kontakt </a></li>-->
+                <!--                    <li><a href="" class="text">Impressum</a></li>-->
+                <!--                    <li><a href="" class="text">Datenschutz</a></li>-->
+                <!--                    <li><a href="" class="text">AGB</a></li>-->
+                <!--                </ul>-->
             </div>
             <div class="col-lg-4 col-md-5 col-sm-6 col-12">
-                <?php dynamic_sidebar( 'footer_area_two' ); ?>
-<!--                <div class="text label">WEITERBILDUNGSPROGRAMME</div>-->
-<!--                <ul>-->
-<!--                    <li><a href="" class="text">Vertriebsleiterausbildung (IHK) </a></li>-->
-<!--                    <li><a href="" class="text">Geprüfter Vertriebsleiter (CEA)</a></li>-->
-<!--                    <li><a href="" class="text">Trainer für betriebliche Weiterbildung (IHK)</a></li>-->
-<!--                    <li><a href="" class="text">Management- und Führungstrainer (IHK)</a></li>-->
-<!--                </ul>-->
+                <?php dynamic_sidebar('footer_area_two'); ?>
+                <!--                <div class="text label">WEITERBILDUNGSPROGRAMME</div>-->
+                <!--                <ul>-->
+                <!--                    <li><a href="" class="text">Vertriebsleiterausbildung (IHK) </a></li>-->
+                <!--                    <li><a href="" class="text">Geprüfter Vertriebsleiter (CEA)</a></li>-->
+                <!--                    <li><a href="" class="text">Trainer für betriebliche Weiterbildung (IHK)</a></li>-->
+                <!--                    <li><a href="" class="text">Management- und Führungstrainer (IHK)</a></li>-->
+                <!--                </ul>-->
             </div>
             <div class="col-lg-3 col-md-4 col-sm-6 col-12">
-                <?php dynamic_sidebar( 'footer_area_three' ); ?>
-<!--                <div class="text label">KONTAKT</div>-->
-<!--                <ul>-->
-<!--                    <li><a href="" class="text">DVKS Deutsche Verkäuferschule® </a></li>-->
-<!--                    <li><a href="" class="text">Mehrhooger Straße 1c</a></li>-->
-<!--                    <li><a href="" class="text">D-46499 Hamminkeln</a></li>-->
-<!--                    <li><a href="tel:4961044099846" class="text">Fon: +49 6104 4099846</a></li>-->
-<!--                    <li><a href="mailto:willkommen@verkaeuferschule.de" class="text">willkommen@verkaeuferschule.de</a>-->
-<!--                    </li>-->
-<!--                </ul>-->
+                <?php dynamic_sidebar('footer_area_three'); ?>
+                <!--                <div class="text label">KONTAKT</div>-->
+                <!--                <ul>-->
+                <!--                    <li><a href="" class="text">DVKS Deutsche Verkäuferschule® </a></li>-->
+                <!--                    <li><a href="" class="text">Mehrhooger Straße 1c</a></li>-->
+                <!--                    <li><a href="" class="text">D-46499 Hamminkeln</a></li>-->
+                <!--                    <li><a href="tel:4961044099846" class="text">Fon: +49 6104 4099846</a></li>-->
+                <!--                    <li><a href="mailto:willkommen@verkaeuferschule.de" class="text">willkommen@verkaeuferschule.de</a>-->
+                <!--                    </li>-->
+                <!--                </ul>-->
             </div>
             <div class="col-12">
                 <div class="rights">
-                    <div class="text">© <?php bloginfo('name'); ?> <?php echo date('Y'); ?>. Alle Rechte vorbehalten.</div>
+                    <div class="text">© <?php bloginfo('name'); ?> <?php echo date('Y').'. '.get_option('copyright'); ?>
+                    </div>
                 </div>
             </div>
         </div>
@@ -57,25 +58,25 @@
 </html>
 
 <!---->
-			<!-- footer -->
+<!-- footer -->
 <!--			<footer class="footer" role="contentinfo">-->
 <!---->
-				<!-- copyright -->
+<!-- copyright -->
 <!--				<p class="copyright">-->
 <!--					&copy; --><?php //echo date('Y'); ?><!-- Copyright --><?php //bloginfo('name'); ?><!--. --><?php //_e('Powered by', 'html5blank'); ?>
 <!--					<a href="//wordpress.org" title="WordPress">WordPress</a> &amp; <a href="//html5blank.com" title="HTML5 Blank">HTML5 Blank</a>.-->
 <!--				</p>-->
-				<!-- /copyright -->
+<!-- /copyright -->
 <!---->
 <!--			</footer>-->
-			<!-- /footer -->
+<!-- /footer -->
 <!---->
 <!--		</div>-->
-		<!-- /wrapper -->
+<!-- /wrapper -->
 <!---->
 <!--		--><?php //wp_footer(); ?>
 
-		<!-- analytics -->
+<!-- analytics -->
 <!--		<script>-->
 <!--		(function(f,i,r,e,s,h,l){i['GoogleAnalyticsObject']=s;f[s]=f[s]||function(){-->
 <!--		(f[s].q=f[s].q||[]).push(arguments)},f[s].l=1*new Date();h=i.createElement(r),-->
--- header.php
+++ header.php
@@ -12,69 +12,6 @@
 
         <?php bloginfo('name'); ?>
     </title>
-<!--    <link rel="shortcut icon" href="--><?php //echo get_template_directory_uri(); ?><!--/img/favicon.png" type="image/x-icon">-->
-<!--    <link rel="stylesheet" href="css/main.min.css">-->
-<!--    <link rel="stylesheet" href="css/animate.min.css">-->
-<!--    <script src="js/wow.min.js"></script>-->
-<!--    <script src="js/main.min.js"></script>-->
-<!--    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.css"/>-->
-<!--    <script src="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.js"></script>-->
     <?php wp_head() ?>
 </head>
 <body>
-<!---->
-<!--<!doctype html>-->
-<!--<html --><?php //language_attributes(); ?><!-- class="no-js">-->
-<!--	<head>-->
-<!--		<meta charset="--><?php //bloginfo('charset'); ?><!--">-->
-<!--		<title>--><?php //wp_title(''); ?><!----><?php //if(wp_title('', false)) { echo ' :'; } ?><!-- -->
-<?php //bloginfo('name'); ?><!--</title>-->
-<!---->
-<!--		<link href="//www.google-analytics.com" rel="dns-prefetch">-->
-<!--        <link href="-->
-<?php //echo get_template_directory_uri(); ?><!--/img/icons/favicon.ico" rel="shortcut icon">-->
-<!--        <link href="-->
-<?php //echo get_template_directory_uri(); ?><!--/img/icons/touch.png" rel="apple-touch-icon-precomposed">-->
-<!---->
-<!--		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">-->
-<!--		<meta name="viewport" content="width=device-width, initial-scale=1.0">-->
-<!--		<meta name="description" content="--><?php //bloginfo('description'); ?><!--">-->
-<!---->
-<!--		--><?php //wp_head(); ?>
-<!--		<script>-->
-<!--        // conditionizr.com-->
-<!--        // configure environment tests-->
-<!--        conditionizr.config({-->
-<!--            assets: '--><?php //echo get_template_directory_uri(); ?>////',
-<!--//            tests: {}-->
-<!--//        });-->
-<!--//        </script>-->
-<!--//-->
-<!--//	</head>-->
-<!--//	<body --><?php ////body_class(); ?><!--<!-->-->-->
-<!---->
-		<!-- wrapper -->
-<!--		<div class="wrapper">-->
-<!---->
-			<!-- header -->
-<!--			<header class="header clear" role="banner">-->
-<!---->
-					<!-- logo -->
-<!--					<div class="logo">-->
-<!--						<a href="--><?php //echo home_url(); ?><!--">-->
-							<!-- svg logo - toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script
--->
-<!--							<img src="-->
-<?php //echo get_template_directory_uri(); ?><!--/img/logo.svg" alt="Logo" class="logo-img">-->
-<!--						</a>-->
-<!--					</div>-->
-					<!-- /logo -->
-<!---->
-					<!-- nav -->
-<!--					<nav class="nav" role="navigation">-->
-<!--						--><?php //html5blank_nav(); ?>
-<!--					</nav>-->
-					<!-- /nav -->
-<!---->
-<!--			</header>-->
-			<!-- /header -->
